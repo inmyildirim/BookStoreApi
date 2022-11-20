e7b44d576d3e7a8264c3361247146fb7c812114c
Mehmet YILDIRIM <97946779+inmyildirim@users.noreply.github.com>
2022-11-20 19:49:01 +0300

    Reusability improvement created

--- src/test/java/demoqa/step_definitions/CreateUserSteps.java
+++ src/test/java/demoqa/step_definitions/CreateUserSteps.java
@@ -1,19 +1,18 @@
 package demoqa.step_definitions;
 
 import demoqa.pages.ResponseApiPage;
+import demoqa.utilities.BookStoreApiUtils;
 import demoqa.utilities.ConfigurationReader;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import static org.junit.Assert.*;
 
-
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
+
 
 import static io.restassured.RestAssured.*;
 
@@ -22,8 +21,10 @@ public class CreateUserSteps {
     String requestUsername;
     String requestPassword;
     String responseUserID;
-    File outFile;
-    PrintWriter outPut;
+    String responseUserName;
+    int responseStatusCode;
+    List<String> books;
+
     ResponseApiPage responsePage = new ResponseApiPage();
 
 
@@ -40,32 +41,22 @@ public class CreateUserSteps {
                 .when().post();
     }
 
-    @When("User captures status code userID username information")
+    @When("User captures status code, userID, username and books information")
     public void user_captures_status_code_userID_username_information() throws FileNotFoundException {
         //we are deserializing response object to get userID
-        Map<Object,Object> responseMap = response.as(Map.class);
+        Map<Object, Object> responseMap = response.as(Map.class);
         responseUserID = (String) responseMap.get("userID");
-        //writing userID to a regular resources file
-        //defining a file: we need to provide path of the file
-        outFile = new File("C:\\Users\\Mehmet\\IdeaProjects\\ApiUpSkillProject\\src\\test\\resources\\features\\userID.out");
-        if (outFile.exists()){
-            outFile.delete();
-        }
-        outPut = new PrintWriter(outFile);
-        outPut.println(responseUserID);
-        outPut.close(); // this step crucial to actually finalize writing function
+        BookStoreApiUtils.storeInfoToFile(responseUserID);
+        responseUserName = (String) responseMap.get("username");
+        books = (List<String>) responseMap.get("books");
+
     }
 
     @Then("Verify status code username and UserID is NOT null")
     public void verify_status_code_username_and_UserID_is_NOT_null() throws FileNotFoundException {
-
-        //read the file for userID
-        outFile = new File("C:\\Users\\Mehmet\\IdeaProjects\\ApiUpSkillProject\\src\\test\\resources\\features\\userID.out");
-
-        Scanner scan = new Scanner(outFile);
-        String userID = scan.next();
-        System.out.println("userID =" +userID);
-
-
+        assertEquals(200,responseStatusCode);
+        assertEquals(requestUsername,responseUserName);
+        assertFalse(responseUserID == null);
     }
+
 }
--- src/test/resources/features/createUser.feature
+++ src/test/resources/features/createUser.feature
@@ -3,5 +3,5 @@ Feature: Books Store API functionality
   @wip
   Scenario: Create a user
     When User sends a POST Request to create user end point
-    And User captures status code userID username information
+    And User captures status code, userID, username and books information
     Then Verify status code username and UserID is NOT null
